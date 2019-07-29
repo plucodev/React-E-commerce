e8caa9451feedbe5f212c75fd55f30cb34a275dc
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-07-29 21:29:15 +0000

    Resolved products page layout

--- src/js/component/productCard.js
+++ src/js/component/productCard.js
@@ -16,28 +16,30 @@ export class ProductCard extends React.Component {
 						return item.sku == this.props.sku;
 					});
 					return (
-						<div className="card text-center px-2">
-							<div className="card-header bg-transparent pb-2 mb-2 text-capitalize">
-								Category: <h6>{product.category}</h6>
-							</div>
-							<img
-								className="card-img-top"
-								src={product.image}
-								alt="Generic placeholder image"
-								width="140"
-								height="140"
-							/>
-							<h5 className="card-title mt-3 text-truncate">{product.name}</h5>
-							<p className="text-truncate font-weight-light">{product.description}</p>
-							<p className="font-weight-bold">{price(product.price)}</p>
-							<Rating stars={product.rating} />
+						<div className="col-4 mb-3">
+							<div className="card text-center px-2">
+								<div className="card-header bg-transparent pb-2 mb-2 text-capitalize">
+									Category: <h6>{product.category}</h6>
+								</div>
+								<img
+									className="card-img-top"
+									src={product.image}
+									alt="Generic placeholder image"
+									width="140"
+									height="140"
+								/>
+								<h5 className="card-title mt-3 text-truncate">{product.name}</h5>
+								<p className="text-truncate font-weight-light">{product.description}</p>
+								<p className="font-weight-bold">{price(product.price)}</p>
+								<Rating stars={product.rating} />
 
-							<div className="card-footer bg-transparent ">
-								<p className="mb-0">
-									<Link className="btn btn-success" to={"/product/" + product.sku} role="button">
-										View details &raquo;
-									</Link>
-								</p>
+								<div className="card-footer bg-transparent ">
+									<p className="mb-0">
+										<Link className="btn btn-success" to={"/product/" + product.sku} role="button">
+											View details &raquo;
+										</Link>
+									</p>
+								</div>
 							</div>
 						</div>
 					);
--- src/js/views/products.js
+++ src/js/views/products.js
@@ -9,13 +9,17 @@ export class Products extends React.Component {
 		return (
 			<div>
 				<div className="h1 text-center mb-4">Products</div>
-				<Context.Consumer className="card-deck">
-					{({ store }) => {
-						return store.products.map((item, index) => {
-							return <ProductCard sku={item.sku} key={index} />;
-						});
-					}}
-				</Context.Consumer>
+				<div className="container">
+					<div className="row">
+						<Context.Consumer>
+							{({ store }) => {
+								return store.products.map((item, index) => {
+									return <ProductCard sku={item.sku} key={index} />;
+								});
+							}}
+						</Context.Consumer>
+					</div>
+				</div>
 			</div>
 		);
 	}
